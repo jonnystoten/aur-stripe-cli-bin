6259c7b817836fd86551bd46980edacb410cbca1
Jonny Stoten <jonny@jonnystoten.com>
2024-05-22 10:05:20 +0100

    Release of 1.19.5-2

--- PKGBUILD
+++ PKGBUILD
@@ -2,7 +2,7 @@
 
 pkgname=stripe-cli-bin
 pkgver=1.19.5
-pkgrel=1
+pkgrel=2
 pkgdesc="A command-line tool for Stripe"
 arch=("x86_64")
 url="https://stripe.com/docs/stripe-cli"
@@ -11,7 +11,7 @@ depends=()
 provides=()
 conflicts=(stripe-cli)
 source=("https://github.com/stripe/stripe-cli/releases/download/v$pkgver/stripe_${pkgver}_linux_x86_64.tar.gz")
-sha256sums=('8840e3705d2bab93df53903311c8fc25fff11fbb93e85035a6d0e4930b8a30a5')
+sha256sums=('12e0d482e4e606f753afd53723b55de35da2aeded7024d29369f021167b79460')
 
 package() {
   install -Dm 0755 stripe "$pkgdir/usr/bin/stripe"
--- release.fish
+++ release.fish
@@ -1,6 +1,11 @@
 #!/usr/bin/env fish
 
 set stripe_cli_version $argv[1]
+if set -q argv[2]
+    set stripe_cli_rel $argv[2]
+else
+    set stripe_cli_rel 1
+end
 
 echo "Making a new release of stripe-cli $stripe_cli_version"
 
@@ -8,7 +13,7 @@ echo "Getting checksum..."
 set checksum (curl -L https://github.com/stripe/stripe-cli/releases/download/v$stripe_cli_version/stripe-linux-checksums.txt | grep linux_x86_64.tar.gz | awk '{print $1}')
 
 echo "Building PKGBUILD..."
-sed "s/VERSION/$stripe_cli_version/g;s/CHECKSUM/$checksum/g" PKGBUILD.template >PKGBUILD
+sed "s/VERSION/$stripe_cli_version/g;s/REL/$stripe_cli_rel/g;s/CHECKSUM/$checksum/g" PKGBUILD.template >PKGBUILD
 
 echo "Making and installing package locally..."
 makepkg -i; or exit
@@ -23,9 +28,9 @@ if not stripe -v
 end
 
 read -p 'echo "Ready to commit/push? [Y/n] "' ok
-if test $ok = "y" -o $ok = ""
+if test $ok = y -o $ok = ""
     echo "Committing to git..."
-    git commit -am "Release of $stripe_cli_version"
+    git commit -am "Release of $stripe_cli_version-$stripe_cli_rel"
 
     echo "Pushing..."
     git push origin master
